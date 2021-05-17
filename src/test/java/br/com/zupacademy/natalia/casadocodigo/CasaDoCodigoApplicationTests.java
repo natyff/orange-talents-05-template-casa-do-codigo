13a67bfd223e13274989562fd1aa3fa214902791
unknown <natyyy@gmail.com>
2021-05-17 12:19:38 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/natalia/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.natalia.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
